5d54738d75d0b25090d37fc0d81a4f9d8ecfbdba
maki <jantulamarko@outlook.com>
2022-12-06 08:30:37 +0100

    updated to 10.04

---
+++ Open-Lab-10.05/Book.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Lab_10._03
+{
+    internal class Book
+    {
+        private string title;
+        private int pages;
+        private string category;
+        private string author;
+        private int releaseDate;
+
+        public string Title
+        { get { return title; } set { title = value; } }
+
+        public int Pages
+        { get { return pages; } set { pages = value; } }
+
+        public string Category
+        { get { return category; } set { category = value; } }
+
+        public int ReleaseDate
+        {
+            get => releaseDate;
+            set
+            {
+                if ((value >= 2021) || (value <= 1450))
+                {
+                    releaseDate = -1;
+                }
+                else
+                {
+                    releaseDate = value;
+                }
+            }
+
+        }
+
+        public string Author
+        { get { return author; } set { author = value; } }
+
+
+        public Book()
+        {
+            title = "-1";
+            pages = -1;
+            category = "-1";
+            author = "-1";
+            releaseDate = -1;
+        }
+
+        public Book(string mtitle, int mpages)
+        {
+            title = mtitle;
+            pages = mpages;
+            category = "-1";
+            author = "-1";
+            releaseDate = -1;
+        }
+
+        public Book(string mtitle, int mpages, string mcategory, string mauthor, int mreleaseDate)
+        {
+            title = mtitle;
+            pages = mpages;
+            category = mcategory;
+            author = mauthor;
+            releaseDate = mreleaseDate;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"{title}\n{pages}\n{category}\n{author}\n{releaseDate}");
+        }
+
+    }
+}
--- Open-Lab-10.05/Program.cs
+++ Open-Lab-10.05/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Open_Lab_10._03;
+using Open_Lab_10._04;
+using Open_Lab_10._05;
+using System;
+using System.Security.Cryptography;
 
-namespace Open_Lab_10._05
+namespace Open_Lab_10._04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Book LOTR = new Book();
+            //LOTR.Title = "The Lord of the Rings";
+            //LOTR.Pages = 9250;
+            //LOTR.Category = "epic high-fantasy novel";
+            //LOTR.Author = "John Ronald Reuel Tolkien";
+            //LOTR.ReleaseDate = 1954;
+            Console.WriteLine(LOTR.ToString());
+
+            Book LOTR2 = new Book();
+            Console.WriteLine(LOTR2.ToString());
+
+            Book LOTR3 = new Book("The Lord of the Rings 3", 576);
+            Console.WriteLine(LOTR3.ToString());
+
+            Book Hobit = new Book("Hobit", 304, "Fantasy", "John Ronald Reuel Tolkien", 1954);
+            Console.WriteLine(Hobit.ToString());
         }
     }
 }
